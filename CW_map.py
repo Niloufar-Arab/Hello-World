7195f8a2f46cace9d0171e2008d91209531ab9c5
Niloufar-Arab <choubdar.niloufar@gmail.com>
2025-07-18 17:12:52 +0330

    add fibo

--- CW_map.py
+++ CW_map.py
@@ -1,33 +1,33 @@
-# def fibo(n):
-#     if n <= 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-#     return fibo(n - 1) + fibo(n - 2)
+def fibo(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibo(n - 1) + fibo(n - 2)
 
-# def fibo_generator(n):
-#     for i in range(n):
-#         yield fibo(i)
+def fibo_generator(n):
+    for i in range(n):
+        yield fibo(i)
 
-# print(num for num in fibo_generator(10))
+print(list(num for num in fibo_generator(10)))
+# print(list(fibo(6)))
 
 
+# import time
+# def time_record(func):
+#     def wrapper():
+#         start=time.time()
+#         function_1=func()
+#         end= time.time()
+#         print(f"time: {end-start}")
+#         return function_1
+#     return wrapper
 
-@time_record
-def gener(seconds):
-    for i in range (seconds):
-        print(i)
-        yield i
-import time
-def time_record(func):
-    def wrapper():
-        start=time.time()
-        function_1=func()
-        end= time.time()
-        print(f"time: {end-start}")
-        return function_1
-    return wrapper
-
+# @time_record
+# def gener(seconds):
+#     for i in range (seconds):
+#         print(i)
+#         yield i
 
 
 

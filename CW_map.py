2ef29b996b5abca1350d2aa73fb5dcfeab9dfa1b
Niloufar-Arab <choubdar.niloufar@gmail.com>
2025-07-18 16:41:30 +0330

    created

---
+++ CW_map.py
@@ -0,0 +1,42 @@
+# def fibo(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     return fibo(n - 1) + fibo(n - 2)
+
+# def fibo_generator(n):
+#     for i in range(n):
+#         yield fibo(i)
+
+# print(num for num in fibo_generator(10))
+
+
+
+@time_record
+def gener(seconds):
+    for i in range (seconds):
+        print(i)
+        yield i
+import time
+def time_record(func):
+    def wrapper():
+        start=time.time()
+        function_1=func()
+        end= time.time()
+        print(f"time: {end-start}")
+        return function_1
+    return wrapper
+
+
+
+
+
+
+
+
+
+
+
+
+
